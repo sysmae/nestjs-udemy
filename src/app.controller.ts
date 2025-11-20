37fcc75f37a9d03c04b6657b62b1df7360e6d345
sysmae <j970330@gmail.com>
2025-11-20 23:06:28 +0900

    feat: update AppController to define base route and additional endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,17 @@
 import { Controller, Get } from "@nestjs/common";
 
-@Controller()
+// 전체 경로: /app/asdf
+// @Controller('/app') 은 AppController의 기본 경로를 설정
+// @Get('/asdf') 는 getRootRoute 메서드의 경로를 설정
+@Controller('/app')
 export class AppController {
-    @Get()
+    @Get('/asdf')
     getRootRoute() {
         return "Hello World!";
+    }
+
+    @Get('/bye')
+    getByeRoute() {
+        return "Goodbye World!";
     }
 }
