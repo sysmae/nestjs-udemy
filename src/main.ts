7971a2fc8b3e4be6234d8638ef3b665618a26465
sysmae <j970330@gmail.com>
2025-11-20 23:00:08 +0900

    feat: initialize NestJS application with a basic controller and TypeScript configuration

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { Controller , Module, Get} from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+
+@Controller()
+export class AppController {
+    @Get()
+    getRootRoute() {
+        return "Hello World!";
+    }
+}
+
+@Module({
+    controllers:[AppController],
+})
+class AppModule{}
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    await app.listen(3000)
+}
+
+bootstrap();
