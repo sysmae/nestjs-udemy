4f556dd47bb038c1ca734300405b6596509c0850
sysmae <j970330@gmail.com>
2025-11-25 21:00:57 +0900

    feat: Import PowerModule into DiskModule and add .DS_Store to .gitignore.

--- di/src/cpu/cpu.service.ts
+++ di/src/cpu/cpu.service.ts
@@ -5,4 +5,10 @@ import { PowerService } from '../power/power.service';
 export class CpuService {
     // 생성자를 통해 PowerService의 인스턴스를 주입받음
     constructor(private powerService: PowerService) {}
+
+    compute(a:number, b:number) {
+        console.log('10와트의 전력을 소모하여 연산을 수행합니다.');
+        this.powerService.supplyPower(10);
+        return a + b;
+    }
 }
--- di/src/disk/disk.service.ts
+++ di/src/disk/disk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+    constructor(private powerService: PowerService) {}
+
+    getData() {
+    console.log('20와트의 전력을 소모하여 데이터를 찾습니다.');
+    this.powerService.supplyPower(20); // 주입받은 서비스 사용
+    return 'Data!';
+  }
+}
