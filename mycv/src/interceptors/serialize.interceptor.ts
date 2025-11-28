a90645cc85b40f557e326de30abb3cf2126cf3ad
sysmae <j970330@gmail.com>
2025-11-28 20:42:19 +0900

    refactor: replace `any` with `ClassConstructor` interface for DTO types in `Serialize` interceptor.

--- mycv/src/interceptors/serialize.interceptor.ts
+++ mycv/src/interceptors/serialize.interceptor.ts
@@ -8,21 +8,25 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
 
+// [1] 클래스 타입 정의
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
 
-export function Serialize(dto: any) {
+// [2] 데코레이터 함수 인자 타입 변경 (any -> ClassConstructor)
+export function Serialize(dto: ClassConstructor) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 export class SerializeInterceptor implements NestInterceptor {
-  constructor(private dto: any) {}
+  // [3] 인터셉터 생성자 인자 타입 변경
+  constructor(private dto: ClassConstructor) {}
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    
     return next.handle().pipe(
       map((data: any) => {
-        // 데이터가 응답으로 나가기 전 실행되는 영역
-        
         return plainToClass(this.dto, data, {
-          excludeExtraneousValues: true, // 핵심 설정!
+          excludeExtraneousValues: true,
         });
       }),
     );
