dffd901aa0fb966509595dce40ad933dee09aa4d
sysmae <j970330@gmail.com>
2025-11-28 20:29:45 +0900

    feat: add UserDto and implement response serialization using plainToClass in SerializeInterceptor.

--- mycv/src/interceptors/serialize.interceptor.ts
+++ mycv/src/interceptors/serialize.interceptor.ts
@@ -1,20 +1,24 @@
-import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from "@nestjs/common";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
-import { plainToClass } from "class-transformer";
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+import { UserDto } from '../users/dtos/user.dto'; // UserDto 임포트
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    // [Phase 1] 요청이 핸들러에 도달하기 전 실행
-    console.log('Im running before the handler', context);
-
+    
     return next.handle().pipe(
       map((data: any) => {
-        // [Phase 2] 응답이 클라이언트로 전송되기 전 실행
-        console.log('Im running before response is sent out', data);
+        // 데이터가 응답으로 나가기 전 실행되는 영역
         
-        // 여기에 직렬화 로직이 들어갈 예정 (ex: plainToClass)
-        return data; 
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true, // 핵심 설정!
+        });
       }),
     );
   }
---
+++ mycv/src/users/dtos/user.dto.ts
@@ -0,0 +1,11 @@
+import { Expose, Exclude } from "class-transformer";
+
+export class UserDto {
+    @Expose()
+    id: number;
+
+    @Expose()
+    email: string;
+
+
+}
